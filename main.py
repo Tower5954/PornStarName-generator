b6bc7de7e4d64e16b4f455dd6359eaa26e8dfd80
Towvis <rvanna@hotmail.co.uk>
2020-11-18 10:58:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Greetings")
+
+first_name = input("What was/is your first pets name?\n")
+
+surname = input("What is your mothers maiden name?\n")
+
+print("Your porn star name is...\n " + first_name + " " + surname)
